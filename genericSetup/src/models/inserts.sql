9ac279773055503a366b21477a89a4de28633588
ChristianCSE <csanch4cs@gmail.com>
2018-07-02 16:58:21 -0700

    fixing sql tables & formatting seeding data

---
+++ genericSetup/src/models/inserts.sql
@@ -0,0 +1,19 @@
+use music; 
+
+load data local infile 'artist.csv' into table artist 
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\';;
+-- LINES TERMINATED BY '\r\n';
+
+load data local infile 'album.csv' into table album 
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\';;;
+-- LINES TERMINATED BY '\r\n';
+
+load data local infile 'song.csv' into table song 
+FIELDS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '\\';;
+-- LINES TERMINATED BY '\r\n';
