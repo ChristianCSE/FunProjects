94cd1830dd9f654cf97e23ab4e0a40b6b0b5ed8b
ChristianCSE <csanch4cs@gmail.com>
2018-07-20 17:42:57 -0700

    more deps & starting async

--- moreRedux/parsnip/src/index.js
+++ moreRedux/parsnip/src/index.js
@@ -1,21 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+//import { devToolsEnhancer } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import tasks from './reducers';
 import App from './App';
 
 import './index.css';
 
-const store = createStore(tasks);
+//const store = createStore(tasks, devToolsEnhancer());
+const store = createStore(
+  tasks,
+  composeWithDevTools( applyMiddleware(thunk) )
+);
 
 const wrapProvider = (
   <Provider store={store}>
     <App />
   </Provider>
 );
+
+//module.hot is exposed in development mode ONLY 
+//rerender the component when the App component changes 
+//or if any of its children change 
+if (module.hot) {
+  //module.hot.accept(one or more deps, callback)
+  //sending string location of our component that will rerender
+  //callback is triggered after the modules have successfully been repalced. 
+
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    console.log('NextApp: ', NextApp);
+    const changes = (
+      <Provider store={store}> 
+        <NextApp />
+      </Provider>
+    );
+    ReactDOM.render(changes, document.getElementById('root'));
+  });
+
+
+  //reducer & store
+  //whenever reducer updates, perform the hot module replacement
+  module.hot.accept('./reducers', () => {
+    const nextRootReducer = require('./reducers').default;
+    store.replaceReducer(nextRootReducer);
+  });
+
+}
 
 
 ReactDOM.render(wrapProvider, document.getElementById('root'));
